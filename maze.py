5bdac0379fad5a9665a40232deba25419afd9573
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-11-12 15:55:38 -0800

    add recursive break walls method to create maze

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 import time
+import random
 from cell import Cell
 
 class Maze():
@@ -11,6 +12,7 @@ class Maze():
         cell_size_x,
         cell_size_y,
         win,
+        seed=None
         ):
         self.x1 = x1
         self.y1 = y1
@@ -19,14 +21,17 @@ class Maze():
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if seed:
+            random.seed(seed)
         self._create_cells()
+        self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
 
     def _create_cells(self):
-        self._cells = [[Cell(self._win) for _ in range(self._num_cols)] for _ in range(self._num_rows)]
+        self._cells = [[Cell(self._win) for x in range(self._num_cols)] for y in range(self._num_rows)]
         for i in range(self._num_rows):
             for j in range(self._num_cols):
                 self._draw_cell(i, j)
-        self._break_entrance_and_exit()
 
     def _draw_cell(self, i, j):
         cell = self._cells[i][j]
@@ -43,6 +48,44 @@ class Maze():
         self._cells[self._num_rows - 1][self._num_cols - 1].has_bottom_wall = False
         self._draw_cell(self._num_rows - 1, self._num_cols - 1)
 
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+            # Left Cell
+            if j > 0 and self._cells[i][j - 1].visited == False:
+                to_visit.append([i, j-1])
+            # Right Cell
+            if j < self._num_cols - 1 and self._cells[i][j + 1].visited == False:
+                to_visit.append([i, j + 1])
+            # Top Cell
+            if i > 0 and self._cells[i - 1][j].visited == False:
+                to_visit.append([i - 1, j])
+            # Bottom Cell
+            if i < self._num_rows - 1 and self._cells[i + 1][j].visited == False:
+                to_visit.append([i + 1, j])
+            if len(to_visit) == 0:
+                self._draw_cell(i, j)
+                return
+            direction = random.choice(to_visit)
+            # I J is above direction
+            if direction[0] > i:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i + 1][j].has_top_wall = False
+            # I J is below direction
+            if direction[0] < i:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i - 1][j].has_bottom_wall = False
+            # I J is to the left of direction
+            if direction[1] > j:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i][j + 1].has_left_wall = False
+            # I J is to the right of direction
+            if direction[1] < j:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i][j - 1].has_right_wall = False
+            self._break_walls_r(direction[0], direction[1])
+            
     def _animate(self):
         self._win.redraw()
         time.sleep(0.05)
