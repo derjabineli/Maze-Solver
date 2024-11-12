5e38638bb3619fac53ec014a76e3b4205b5ddefa
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-11-12 13:45:38 -0800

    add method to create entrance and exit cells

--- maze.py
+++ maze.py
@@ -14,28 +14,35 @@ class Maze():
         ):
         self.x1 = x1
         self.y1 = y1
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.cell_size_x = cell_size_x
-        self.cell_size_y = cell_size_y
-        self.win = win
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
         self._create_cells()
 
     def _create_cells(self):
-        self._cells = [[Cell(self.win) for _ in range(self.num_cols)] for _ in range(self.num_rows)]
-        for i in range(self.num_rows):
-            for j in range(self.num_cols):
+        self._cells = [[Cell(self._win) for _ in range(self._num_cols)] for _ in range(self._num_rows)]
+        for i in range(self._num_rows):
+            for j in range(self._num_cols):
                 self._draw_cell(i, j)
+        self._break_entrance_and_exit()
 
     def _draw_cell(self, i, j):
         cell = self._cells[i][j]
-        x1 = self.x1 + (j * self.cell_size_x)
-        y1 = self.y1 + (i * self.cell_size_y)
-        x2 = x1 + self.cell_size_x
-        y2 = y1 + self.cell_size_y
+        x1 = self.x1 + (j * self._cell_size_x)
+        y1 = self.y1 + (i * self._cell_size_y)
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
         cell.draw(x1, y1, x2, y2)
         self._animate()
+    
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_left_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_rows - 1][self._num_cols - 1].has_bottom_wall = False
+        self._draw_cell(self._num_rows - 1, self._num_cols - 1)
 
     def _animate(self):
-        self.win.redraw()
+        self._win.redraw()
         time.sleep(0.05)
