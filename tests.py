af824809c291b2dcf31766d2525e7126e3457aac
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-11-12 16:09:09 -0800

    remove comments

--- tests.py
+++ tests.py
@@ -3,62 +3,61 @@ from graphics import Window
 from maze import Maze
 
 class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        screen_x = 800
+        screen_y = 600
+        win = Window(screen_x, screen_y)
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
+        self.assertEqual(
+            len(m1._cells),
+            num_rows,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_cols,
+        )
     
-    # def test_maze_create_cells(self):
-    #     screen_x = 800
-    #     screen_y = 600
-    #     win = Window(screen_x, screen_y)
-    #     num_cols = 12
-    #     num_rows = 10
-    #     m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
-    #     self.assertEqual(
-    #         len(m1._cells),
-    #         num_rows,
-    #     )
-    #     self.assertEqual(
-    #         len(m1._cells[0]),
-    #         num_cols,
-    #     )
+    def test_maze_create_cells1(self):
+        screen_x = 600
+        screen_y = 600
+        win = Window(screen_x, screen_y)
+        num_cols = 10
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
+        self.assertEqual(
+            len(m1._cells),
+            num_rows,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_cols,
+        )
     
-    # def test_maze_create_cells1(self):
-    #     screen_x = 600
-    #     screen_y = 600
-    #     win = Window(screen_x, screen_y)
-    #     num_cols = 10
-    #     num_rows = 10
-    #     m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
-    #     self.assertEqual(
-    #         len(m1._cells),
-    #         num_rows,
-    #     )
-    #     self.assertEqual(
-    #         len(m1._cells[0]),
-    #         num_cols,
-    #     )
-    
-    # def test_maze_entrance(self):
-    #     screen_x = 800
-    #     screen_y = 600
-    #     win = Window(screen_x, screen_y)
-    #     num_cols = 12
-    #     num_rows = 10
-    #     m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
-    #     self.assertEqual(
-    #         m1._cells[0][0].has_left_wall,
-    #         False,
-    #     )
+    def test_maze_entrance(self):
+        screen_x = 800
+        screen_y = 600
+        win = Window(screen_x, screen_y)
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
+        self.assertEqual(
+            m1._cells[0][0].has_left_wall,
+            False,
+        )
 
-    # def test_maze_exit(self):
-    #     screen_x = 800
-    #     screen_y = 600
-    #     win = Window(screen_x, screen_y)
-    #     num_cols = 12
-    #     num_rows = 10
-    #     m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
-    #     self.assertEqual(
-    #         m1._cells[9][11].has_bottom_wall,
-    #         False,
-    #     )
+    def test_maze_exit(self):
+        screen_x = 800
+        screen_y = 600
+        win = Window(screen_x, screen_y)
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
+        self.assertEqual(
+            m1._cells[9][11].has_bottom_wall,
+            False,
+        )
     def test_visited_reset(self):
         screen_x = 800
         screen_y = 600
